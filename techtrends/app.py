2415a96df2e083d8988e18bc6b89dd62782c98dc
Uwe <uwe.suelter@gmx.net>
2025-03-12 11:14:20 +0100

    Adding date for log output

--- techtrends/app.py
+++ techtrends/app.py
@@ -117,5 +117,9 @@ def metrics():
 
 # start the application on port 3111
 if __name__ == "__main__":
-   logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+   logging.basicConfig(
+       stream=sys.stdout,
+       format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+       datefmt='%Y-%m-%d %H:%M:%S',
+       level=logging.DEBUG)
    app.run(host='0.0.0.0', port='3111')
